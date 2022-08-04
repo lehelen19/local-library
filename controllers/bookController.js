fc9ae6993aa211135367197414c7ff80cbe4b856
Helen Le <le.helen19@gmail.com>
2022-08-04 13:09:44 -0700

    Create Express project with generator. Set up catalog router. Create pug views. Write all read-only controllers and views.
    Database: MongoDB. Models are Author, Book, BookInstance, and Genre.
    Populate the database with sample data in a JS file.
    Redirect index ('/') route to catalog as home page.
    Create the different views for each read-only* page (write pages will be
    implemented later).
    Write the controllers to implement the views for corresponding routes.

---
+++ controllers/bookController.js
@@ -0,0 +1,101 @@
+/* eslint-disable consistent-return */
+/* eslint-disable camelcase */
+/* eslint-disable max-len */
+const async = require('async');
+const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+
+exports.index = function (req, res) {
+  async.parallel({
+    book_count(callback) {
+      Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+    },
+    book_instance_count(callback) {
+      BookInstance.countDocuments({}, callback);
+    },
+    book_instance_available_count(callback) {
+      BookInstance.countDocuments({ status: 'Available' }, callback);
+    },
+    author_count(callback) {
+      Author.countDocuments({}, callback);
+    },
+    genre_count(callback) {
+      Genre.countDocuments({}, callback);
+    },
+  }, (err, results) => {
+    res.render('index', { title: 'Local Library Home', error: err, data: results });
+  });
+};
+
+// Display list of all Books.
+exports.book_list = function (req, res, next) {
+  Book.find({}, 'title author')
+    .sort({ title: 1 })
+    .populate('author')
+    .exec((err, list_books) => {
+      if (err) { return next(err); }
+      // Successful, so render
+      res.render('book_list', { title: 'Book List', book_list: list_books });
+    });
+};
+
+// Display detail page for a specific book.
+exports.book_detail = function (req, res, next) {
+  async.parallel({
+    book(callback) {
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance(callback) {
+      BookInstance.find({ book: req.params.id })
+        .exec(callback);
+    },
+  }, (err, results) => {
+    if (err) { return next(err); }
+    if (results.book == null) { // No results.
+      const error = new Error('Book not found');
+      error.status = 404;
+      return next(error);
+    }
+    // Successful, so render.
+    res.render('book_detail', {
+      title: results.book.title,
+      book: results.book,
+      book_instances: results.book_instance,
+    });
+  });
+};
+
+// Display book create form on GET.
+exports.book_create_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book create GET');
+};
+
+// Handle book create on POST.
+exports.book_create_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book create POST');
+};
+
+// Display book delete form on GET.
+exports.book_delete_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book delete GET');
+};
+
+// Handle book delete on POST.
+exports.book_delete_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book delete POST');
+};
+
+// Display book update form on GET.
+exports.book_update_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book update GET');
+};
+
+// Handle book update on POST.
+exports.book_update_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Book update POST');
+};
---
+++ models/genre.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const GenreSchema = new Schema(
+  {
+    name: {
+      type: String, required: true, minLength: 3, maxLength: 100,
+    },
+  },
+);
+
+// Virtual for bookinstance's URL
+GenreSchema
+  .virtual('url')
+  .get(function () {
+    return `/catalog/genre/${this._id}`;
+  });
+
+// Export model
+module.exports = mongoose.model('Genre', GenreSchema);
